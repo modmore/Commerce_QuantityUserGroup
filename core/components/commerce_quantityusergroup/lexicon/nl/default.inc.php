5ff386795f741489e19499bdd3d1e498bd5bd782
Mark Hamstra <hello@markhamstra.com>
2023-09-25 12:22:56 +0200

    Dutch lexicon

---
+++ core/components/commerce_quantityusergroup/lexicon/nl/default.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+$_lang['commerce_quantityusergroup'] = 'Price Type: Quantity-UserGroup';
+$_lang['commerce_quantityusergroup.description'] = 'Voegt een bulk prijstype toe met extra filter op gebruikersgroep.';
+$_lang['commerce_quantityusergroup.price_type'] = 'Bulk prijzen (gebruikersgroep)';
